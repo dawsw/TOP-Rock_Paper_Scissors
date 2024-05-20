5eba014641e70859a6e2f028ff96840abb9bb70b
dawsw <123456789+odin@users.noreply.github.com>
2024-05-20 01:20:29 -0500

    Created getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+function getComputerChoice(number) {
+    let choice;
+    let result = Math.random(number)
+
+    if (result == 0) {
+         return choice = "Rock";
+    }
+    else if (result == 1) {
+        return choice = "Paper";
+    }
+    else {
+        return choice = "Scissors";
+    }
+}
