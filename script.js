907da597f24b116b3c6fc205bd0ec14bd44c07c8
dawsw <123456789+odin@users.noreply.github.com>
2024-06-01 16:39:37 -0500

    Removed disableButtons/enableButtons functions

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 
 function getComputerChoice(number) {
     let result = Math.floor(Math.random() * number);
@@ -20,107 +17,113 @@ function getComputerChoice(number) {
     }
 }
 
-
-function getHumanChoice() {
-    let rockButton = document.getElementById("rock");
-    let paperButton = document.getElementById("paper");
-    let scissorButton = document.getElementById("scissors");
-    let humanImage = document.querySelector("#player-choice");
-    let humanChoice;
-
-    rockButton.addEventListener("click", () => {
-        humanChoice = "rock";
-        humanImage.src = "images/rock.png";
-        playRound(humanChoice, getComputerChoice(3));
-    
-    });
-
-    paperButton.addEventListener("click", () => {
-        humanChoice = "paper";
-        humanImage.src = "images/rock.png";
-        playRound(humanChoice, getComputerChoice(3));
-    });
-
-    scissorButton.addEventListener("click", () => {
-        humanChoice = "scissors";
-        humanImage.src = "images/scissors.png";
-        playRound(humanChoice, getComputerChoice(3));
-    });
-
-}
-
-
 function playRound(humanChoice, computerChoice) {
     let humanScore = document.querySelector("#player-score");
     let computerScore = document.querySelector("#computer-score");
     let humanTotal = Number(humanScore.textContent);
     let computerTotal = Number(computerScore.textContent);
 
+    let winner = document.getElementById("winner-announcement");
+    let playagainButton = document.querySelector("#playagain-button");
+
     switch(humanChoice + computerChoice) {
         case 'rockpaper':
             computerTotal += 1;
             computerScore.textContent = computerTotal;
-            console.log("You lose! Paper beats Rock.");
+            if (computerScore.textContent == 5) {
+                winner.textContent = "You lose. Better luck next time!";
+                playagainButton.removeAttribute("hidden");
+                playagainButton.style.display = "block";
+            }
             break;
         case 'paperscissors':
             computerTotal += 1;
             computerScore.textContent = computerTotal;
-            console.log("You lose! Scissors beats Paper.");
+            if (computerScore.textContent == 5) {
+                winner.textContent = "You lose. Better luck next time!";
+                playagainButton.removeAttribute("hidden");
+                playagainButton.style.display = "block";
+            }
             break;
         case 'scissorsrock':
             computerTotal += 1;
             computerScore.textContent = computerTotal;
-            console.log("You lose! Rock beats Scissors.");
+            if (computerScore.textContent == 5) {
+                winner.textContent = "You lose. Better luck next time!";
+                playagainButton.removeAttribute("hidden");
+                playagainButton.style.display = "block";
+            }
             break;
         case 'rockscissors':
             humanTotal += 1;
             humanScore.textContent = humanTotal;
-            console.log("You Win! Rock beats Scissors.");
+            if (humanScore.textContent == 5) {
+                winner.textContent = "You Win!";
+                playagainButton.removeAttribute("hidden");
+                playagainButton.style.display = "block";
+            }
             break;
         case 'paperrock':
             humanTotal += 1;
             humanScore.textContent = humanTotal;
-            console.log("You Win! Paper beats Rock.");
+            if (humanScore.textContent == 5) {
+                winner.textContent = "You Win!";
+                playagainButton.removeAttribute("hidden");
+                playagainButton.style.display = "block";
+            }
             break;
         case 'scissorspaper':
             humanTotal += 1;
             humanScore.textContent = humanTotal;
-            console.log("You Win! Scissors beats Paper.");
+            if (humanScore.textContent == 5) {
+                winner.textContent = "You Win!";
+                playagainButton.removeAttribute("hidden");
+                playagainButton.style.display = "block";
+            }
             break;
-        default:
-            console.log("It's a Tie!");
     }
 }
 
 function playGame() {
-    let humanScore = document.querySelector("#player-score");
-    let computerScore = document.querySelector("#computer-score");
-    let humanTotal = Number(humanScore.textContent);
-    let computerTotal = Number(computerScore.textContent);
-    let winner = document.getElementById("winner-announcement");
+    let rockButton = document.getElementById("rock");
+    let paperButton = document.getElementById("paper");
+    let scissorButton = document.getElementById("scissors");
+
     let humanImage = document.querySelector("#player-choice");
-    let computerImage = document.querySelector("#computer-choice");
+    let clickImageText = document.querySelector("#click-image-text");
 
-    if (humanTotal == 5 || computerTotal == 5) {
-        if (computerTotal === 5) {
-            winner.textContent = "You Lose. Better luck next time!";
-        }     
-        else { 
-            winner.textContent = "You Win!";
-        }
-        
-        return;
-    }
+    let humanChoice;
 
-    getHumanChoice();
+    rockButton.addEventListener("click", () => {
+        humanChoice = "rock";
+        humanImage.src = "images/rock.png";
+        clickImageText.style.display = "none";
+        playRound(humanChoice, getComputerChoice(3));
+    
+    });
+
+    paperButton.addEventListener("click", () => {
+        humanChoice = "paper";
+        humanImage.src = "images/paper.png";
+        clickImageText.style.display = "none";
+        playRound(humanChoice, getComputerChoice(3));
+    });
+
+    scissorButton.addEventListener("click", () => {
+        humanChoice = "scissors";
+        humanImage.src = "images/scissors.png";
+        clickImageText.style.display = "none";
+        playRound(humanChoice, getComputerChoice(3));
+    });
+
 }
-
 
 function restartGame() {
     document.querySelector("#player-score").textContent = "0";
     document.querySelector("#computer-score").textContent = "0";
     document.getElementById("playagain-button").style.display = "none";
     document.querySelector("#winner-announcement").textContent = "";
+    document.querySelector("#click-image-text").style.display = "block";
 }
 
 document.getElementById("playagain-button").addEventListener("click", restartGame);
